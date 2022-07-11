dec6b17cc0255e8c02fb2be7c5f130c1d7cf0247
lucas501 <lucas.lelann@gmail.com>
2022-07-11 07:16:16 +0000

    test

--- main.R
+++ main.R
@@ -1,17 +1,15 @@
 library(tercen)
 library(dplyr)
 
-
 ctx = tercenCtx()
-
 multiply_factor = ifelse(is.null(ctx$op.value('multiply_factor')), 1, as.numeric(ctx$op.value('multiply_factor')))
 
 df.out<-ctx %>% 
   select(.y, .ri, .ci) %>% 
   group_by(.ri, .ci) %>%
   mutate(value = multiply_factor*.y) %>%
+  as.data.frame(.) %>%
   ctx$addNamespace()
   
 df.out %>%
   ctx$save()
-
